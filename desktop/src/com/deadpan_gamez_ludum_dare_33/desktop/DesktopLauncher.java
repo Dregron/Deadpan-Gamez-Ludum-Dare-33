e8d8bce5be98b8ddfffd0efbb31579d5970a72ab
Andrew Murray <Andycassidy2007@hotmail.co.uk>
2015-08-17 22:10:24 +0100

    Dregron | code |  first gradle commit

---
+++ desktop/src/com/deadpan_gamez_ludum_dare_33/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.deadpan_gamez_ludum_dare_33.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.deadpan_gamez_ludum_dare_33.Display;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Display(), config);
+	}
+}
---
+++ html/src/com/deadpan_gamez_ludum_dare_33/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.deadpan_gamez_ludum_dare_33.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.deadpan_gamez_ludum_dare_33.Display;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Display();
+        }
+}
